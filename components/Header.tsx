1116b6d8f01c811cbe59be803a856ad96c147acf
Beaskyy <afeez.akintobi@yahoo.com>
2024-08-09 20:41:03 +0100

    update

--- components/Header.tsx
+++ components/Header.tsx
@@ -17,7 +17,7 @@ const Header = ({ headerText }: { headerText: string }) => {
   const { activeMenu, setActiveMenu } = useStateContext();
   return (
     <header
-      className={`fixed  z-20 w-full bg-white transition-all duration-300 ${
+      className={`fixed  z-20 w-full bg-[#FAFBFC] transition-all duration-300 ${
         activeMenu ? "lg:left-[252px] lg:w-custom" : "lg:left-0"
       }`}
     >
